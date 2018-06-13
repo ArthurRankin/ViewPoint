fdc1e2e114a12b57fa4dea3c11269af6017a3ae4
Arthur Smith <arthursmith@ms-MacBook-Pro.attlocal.net>
2018-06-12 22:33:54 -0500

    added intro to login page for usability, and messages and chatinput files to chatroom folder

--- src/components/DBInteractions/base.js
+++ src/components/DBInteractions/base.js
@@ -3,9 +3,12 @@ import firebase from 'firebase';
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyB_eK6WQp6dX61gO1PDnRMnBQKYC6Jh2tg",
-  authDomain: "finalstone-7ef90.firebaseapp.com",
-  databaseURL: "https://finalstone-7ef90.firebaseio.com"
+    apiKey: "AIzaSyB_eK6WQp6dX61gO1PDnRMnBQKYC6Jh2tg",
+    authDomain: "finalstone-7ef90.firebaseapp.com",
+    databaseURL: "https://finalstone-7ef90.firebaseio.com",
+    projectId: "finalstone-7ef90",
+    storageBucket: "finalstone-7ef90.appspot.com",
+    messagingSenderId: "85580365081"
 });
 export const rebase = Rebase.createClass(app.database());
 
--- src/components/loginPage/LoginTemp.js
+++ src/components/loginPage/LoginTemp.js
@@ -4,13 +4,13 @@ import React from 'react';
 let LoginTemp = (props) => {
     return(
         <div>
-            <div className="card text-center ">
+            <div className="card text-center border border-success">
                 <div className="card-header pt-pr-pl-5 pb-0">
                     <h1>ViewPoint</h1>
                 </div>
                 <div className="card-body p-5">
-                    <p>Login With Google</p>
-                    <button onClick={props.function} className="btn btn-primary ">Google</button>
+                    <p>Welcome to ViewPoint! The game that helps you build empathy while simultaneously developing your skills in persuasion. Simply login with <button onClick={props.function} className=" p-0 btn btn-success">Google</button> and answer the questions on the next page. 
+                    </p>
                 </div>
             </div>
         </div>
